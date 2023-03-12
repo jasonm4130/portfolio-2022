fafd5e013ea94542e1e594abc47c0f1af043820a
Jason Matthew <jasonm4130@gmail.com>
2023-03-12 18:36:25 +1000

    fixed issue with build

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PROJECTS_PATH } from '../../lib/consts';
 import getAllFrontmatter from '../../lib/getAllFrontmatter';
-import ProjectCard from '../../components/Projects/ProjectCard';
+import ProjectCard from '../../components/ProjectCard';
 import styles from './projects.module.scss';
 import { MarkdownFileContent } from '../../lib/interfaces';
 
